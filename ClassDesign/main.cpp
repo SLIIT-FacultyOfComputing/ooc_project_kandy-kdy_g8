71aa667b6f260511eda4de4fc3b010e21573dc22
IT21038778 <87402829+IT21038778@users.noreply.github.com>
2021-10-14 17:34:20 +0530

    Update main.cpp

--- ClassDesign/main.cpp
+++ ClassDesign/main.cpp
@@ -1,16 +1,15 @@
 #include <iostream>
-#include "user.h"
-#include "payment.h"
-
+#include "user.h"//includes user class
+#include "payment.h"//includes payment class
 
 using namespace std;
 
-int main()
+int main()//start of main function
 {
   //create static objects
   payment transaction_1;
   admin admin1;
   customer customer1;
 
-  return 0;
+  return 0;//termination of main
 }
